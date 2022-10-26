94c9f95e5eba7b6f3768c7415a9286e0f693b41f
Festanny <60522731+Festanny@users.noreply.github.com>
2022-10-26 12:52:28 +0400

    first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'purple-button': '#4F3ADA;',
+        'purple-text': '#312580;',
+      },
+    },
+  },
+  plugins: [],
+}
